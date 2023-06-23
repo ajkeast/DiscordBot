4dbf5c7815611d409284273f3726a6c0d19aff79
Alexander Keast <94143736+ajkeast@users.noreply.github.com>
2023-06-23 17:31:35 +0000

    Adding function call for datetime to ChatGPT API

--- bot.py
+++ bot.py
@@ -155,20 +155,40 @@ def append_and_shift(arr, v, max_len):
         arr.pop(1)
 
 def call_chatGPT(chat_history, prompt):
-    """
-    Call ChatGPT API with the user prompt and the last 10 messages of  
-    chat history
-    """
-
-    append_and_shift(chat_history,{"role": "user", "content": prompt},max_len=10)
+    """call ChatGPT API and with error handeling blocks"""
     try:
+        append_and_shift(chat_history,{"role": "user", "content": prompt},max_len=10)
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613",
                                                 temperature=0.7,
-                                                messages=chat_history)
-        append_and_shift(chat_history,{"role": "assistant", "content": response['choices'][0]['message']['content']},max_len=10)
-        return chat_history,response['choices'][0]['message']['content'][:2000] # limited to 2000 characters for discord
+                                                messages=chat_history,
+                                                functions=function_descriptions,
+                                                function_call="auto")
+        
+        if response["choices"][0]["finish_reason"] != "function_call":
+            append_and_shift(chat_history,{"role": "assistant", "content": response['choices'][0]['message']['content']},max_len=10)
+        
+        # If there was a function call, append it to the message history and 
+        while response["choices"][0]["finish_reason"] == "function_call":
+            function_response = function_call(response)
+            append_and_shift(chat_history,{"role": "function","name": response["choices"][0]["message"]["function_call"]["name"],"content": json.dumps(function_response)},max_len=10)
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613",
+                                                    temperature=0.7,
+                                                    messages=chat_history,
+                                                    functions=function_descriptions,
+                                                    function_call="auto"
+            )   
+        return chat_history, response['choices'][0]['message']['content'][:2000] # limited to 2000 characters for discord
     except Exception as e:
         return f'Looks like there was an error: {e}'
+    
+def function_call(ai_response):
+    function_call = ai_response["choices"][0]["message"]["function_call"]
+    function_name = function_call["name"]
+    arguments = function_call["arguments"]
+    if function_name == "get_todays_date":
+        return get_todays_date()
+    else:
+        return
 
 def connect_db():
     # connect to database
