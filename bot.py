7818e6c1bc308c0c0813baffeff1e0e5b0808e77
Alexander Keast <94143736+ajkeast@users.noreply.github.com>
2024-01-05 00:21:30 +0000

    Correcting syntax

--- bot.py
+++ bot.py
@@ -82,12 +82,12 @@ async def stats(ctx,*, args=None, pass_context=True, brief='Get an individual us
 async def donation(ctx, brief='Get a list of all donations'):
     # provides embed of all donations
     embed=discord.Embed(title='Donation Board',description='Thank you to our generous patrons!',color=0x4d4170)
-    embed.add_field(name='Matt',value=f'${(6.00+8.91):.2f}',inline=False)
-    embed.add_field(name='Danny E',value=f'${(8.00 + 6.90):.2f}',inline=False)
-    embed.add_field(name='Mike S',value=f'${(8.01):.2f}',inline=False)
-    embed.add_field(name='Sammy T',value=f'${(6.90):.2f}'inline=False)
-    embed.add_field(name='Jacky P',value=f'${(6.69):.2f}'inline=False)
-    embed.add_field(name='Whike',value=f'${(6.00):.2f}'inline=False)
+    embed.add_field(name='Matt',value=f'${(6.00+8.91):.2f}', inline=False)
+    embed.add_field(name='Danny E',value=f'${(8.00 + 6.90):.2f}', inline=False)
+    embed.add_field(name='Mike S',value=f'${(8.01):.2f}', inline=False)
+    embed.add_field(name='Sammy T',value=f'${(6.90):.2f}', inline=False)
+    embed.add_field(name='Jacky P',value=f'${(6.69):.2f}', inline=False)
+    embed.add_field(name='Whike',value=f'${(6.00):.2f}', inline=False)
     embed.set_footer(text='Peter Dinklage is a non-profit')
     await ctx.channel.send(embed=embed)    
 
