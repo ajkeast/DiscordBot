2cae276bb208022e74bbf926a9ffc9d18f20d94f
Alex Keast <alexander.keast@gmail.com>
2024-09-27 23:55:55 -0400

    Parametrized gpt model name, temp, and tokens

--- chatgpt_functions.py
+++ chatgpt_functions.py
@@ -39,9 +39,13 @@ def call_chatGPT(chat_history, prompt):
         append_and_shift(chat_history, {"role": "user", "content": prompt}, max_len=20)
         
         # Send request to the ChatGPT API
-        response = client.chat.completions.create(model="gpt-4o-mini",
-                                                  temperature=0.7,
-                                                  max_tokens=512,
+        model = "gpt-4o-mini"
+        temperature = 0.7
+        max_tokens=512
+
+        response = client.chat.completions.create(model=model,
+                                                  temperature=temperature,
+                                                  max_tokens=max_tokens,
                                                   messages=chat_history,
                                                   functions=function_descriptions,
                                                   function_call="auto")
@@ -54,9 +58,9 @@ def call_chatGPT(chat_history, prompt):
         while response.choices[0].finish_reason == "function_call":
             function_response = function_call(response)
             append_and_shift(chat_history, {"role": "function", "name": response.choices[0].message.function_call.name, "content": json.dumps(function_response)}, max_len=10)
-            response = client.chat.completions.create(model="gpt-3.5-turbo-0613",
-                                                      temperature=0.7,
-                                                      max_tokens=256,
+            response = client.chat.completions.create(model=model,
+                                                      temperature=temperature,
+                                                      max_tokens=max_tokens,
                                                       messages=chat_history,
                                                       functions=function_descriptions,
                                                       function_call="auto")
